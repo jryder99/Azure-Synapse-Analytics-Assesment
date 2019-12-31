33f98946fe750eba4033413d4722084bee7af9f5
jryder99 <jasen.ryder@gmail.com>
2019-12-31 11:17:09 -0600

    Create RowsPerPartitonPerTable.sql

---
+++ RowsPerPartitonPerTable.sql
@@ -0,0 +1,14 @@
+
+SELECT o.name, pnp.index_id, pnp.partition_id, pnp.rows,   
+    pnp.data_compression_desc, pnp.pdw_node_id  
+FROM sys.pdw_nodes_partitions AS pnp  
+JOIN sys.pdw_nodes_tables AS NTables  
+    ON pnp.object_id = NTables.object_id  
+AND pnp.pdw_node_id = NTables.pdw_node_id  
+JOIN sys.pdw_table_mappings AS TMap  
+    ON NTables.name = TMap.physical_name 
+    AND substring(TMap.physical_name,40, 10) = pnp.distribution_id 
+JOIN sys.objects AS o  
+    ON TMap.object_id = o.object_id  
+--WHERE o.name = 'myTable'  
+ORDER BY o.name, pnp.index_id, pnp.partition_id;  
